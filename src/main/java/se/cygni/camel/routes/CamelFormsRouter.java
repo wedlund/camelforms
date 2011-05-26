aa26422202cefb59673bb6bd9de9a2c78e80e2e1
Rebecka Wedlund <rebecka@rwk.se>
2011-05-26 16:05:14 +0200

    Servlet working

--- src/main/java/se/cygni/camel/routes/CamelFormsRouter.java
+++ src/main/java/se/cygni/camel/routes/CamelFormsRouter.java
@@ -9,7 +9,9 @@ public class CamelFormsRouter extends SpringRouteBuilder {
 	@Override
 	public void configure() throws Exception {
 
-//		from().to(activemq:queue);
+//		from("servlet:///hello").to("activemq:queue:destinationQueue").transform().constant("OK");
+		from("servlet:///camelform").transform().constant("You prefer camelforms");
+		from("servlet:///hello").transform().constant("You prefer hello");
 		
 	}
 
